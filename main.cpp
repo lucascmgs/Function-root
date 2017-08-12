bc93bff71c3a69141c5716fef84f16667c5b6bf9
Lucas Cordeiro <lucascmgsds@gmail.com>
2017-08-12 02:13:15 -0300

    Acha o valor de log(2) O programa recebe o inicio e fim do intervalo, além de uma potência de 10 que serve para a tolerância da resposta

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+double funcaoDesejada(double x){
+    return exp(x) - 2 ;
+}
+
+double zeroFunction(double inicio, double fim, double tolerancia){
+    double meio = (fim - inicio) /2, estimativa;
+    while(abs(fim - inicio) > tolerancia) {
+        meio = (fim + inicio)/2;
+        estimativa = funcaoDesejada(meio);
+
+        if(estimativa < 0) {
+            inicio = meio;
+        } else {
+            fim = meio;
+        }
+    }
+    return meio;
+
+}
+
+
+
+int main() {
+    cout << "Insira o inicio, depois o fim do intervalo, e então a potencia da tolerancia " << endl;
+    double inicio, fim, potencia;
+    cin >> inicio >> fim >> potencia;
+
+    double aproximacao = zeroFunction(inicio, fim, pow(10, potencia));
+
+    cout << "Aproximação para o zero da função: " << setprecision(20) << aproximacao << endl;
+    return 0;
+}
