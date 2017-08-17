ed057304f4da68aa8427c43cd3fc69526e498b53
Lucas Cordeiro <lucascmgsds@gmail.com>
2017-08-17 10:41:55 -0300

    tentei fazer alguma coisa

--- main.cpp
+++ main.cpp
@@ -3,16 +3,19 @@
 #include <cmath>
 using namespace std;
 
-double funcaoDesejada(double param, double numero){
-    return exp(param) - numero ;
+
+const double massa = 0.25, coeficiente = 0.1, s0 = 300, gravidade = 32.17, tolerancia = 0.00001;
+
+double funcaoDesejada(double tempo){
+    return (s0 - (massa*gravidade*tempo)/coeficiente +(pow(massa, 2)*gravidade*(1-exp(((-1)*coeficiente*tempo)/massa))/pow(coeficiente, 2)));
 }
 
-double zeroFunction(double inicio, double fim, double tolerancia, double numero){
+double zeroFunction(double inicio, double fim){
     double meio = (fim - inicio) /2, estimativa;
     while(abs(fim - inicio) > tolerancia) {
         meio = (fim + inicio)/2;
-        estimativa = funcaoDesejada(meio, numero);
-
+        estimativa = funcaoDesejada(meio);
+        cout << meio << endl;
         if(estimativa < 0) {
             inicio = meio;
         } else {
@@ -26,13 +29,13 @@ double zeroFunction(double inicio, double fim, double tolerancia, double numero)
 
 
 int main() {
-    cout << "Insira o inicio, depois o fim do intervalo de teste, depois a potencia da tolerancia, depois o numero a se tirar o log " << endl;
-    cout << "Exemplo: 0 100 -8 2" << endl;
+    cout << "Insira o inicio, depois o fim do intervalo de teste" << endl;
+    cout << "Exemplo: 0 100" << endl;
     cout << "É importante saber que se o resultado nao estiver dentro do intervalo a resposta nao fica correta!" << endl;
-    double inicio, fim, potencia, numero;
-    cin >> inicio >> fim >> potencia >> numero;
+    double inicio, fim;
+    cin >> inicio >> fim;
 
-    double aproximacao = zeroFunction(inicio, fim, pow(10, potencia), numero);
+    double aproximacao = zeroFunction(inicio, fim);
 
     cout << "Aproximação para o zero da função: " << setprecision(20) << aproximacao << endl;
     return 0;
